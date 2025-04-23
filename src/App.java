599f99d805312a62b350704c6faa71e3ac78329b
Vincent Guillet <vincent.guillet@outlook.com>
2025-04-23 18:00:21 +0200

    rework

--- src/App.java
+++ src/App.java
@@ -1,36 +1,32 @@
-import brasserie.model.Blonde;
-import java.util.ArrayList;
+import brasserie.Brasserie;
+import brasserie.model.Beer;
+import brasserie.model.Couleur;
+import brasserie.model.Recette;
+
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        List<String> biereDuDemonIngredients = new ArrayList<>();
-        biereDuDemonIngredients.add("Eau");
-        biereDuDemonIngredients.add("Malte d'orge");
-        biereDuDemonIngredients.add("Maïs");
-        biereDuDemonIngredients.add("Riz");
-        biereDuDemonIngredients.add("Extraits de Houblon");
-        biereDuDemonIngredients.add("Houblon");
-        Blonde biereDuDemon = new Blonde("Bière du Démon", "Bière Forte", 12, 3.10, biereDuDemonIngredients);
-        System.out.println("\n"+biereDuDemon+"\n");
+        Brasserie brasserie = new Brasserie("Brasserie R5");
 
-        List<String> grimBlondeIngredients = new ArrayList<>();
-        grimBlondeIngredients.add("Eau");
-        grimBlondeIngredients.add("Malte d'orge");
-        grimBlondeIngredients.add("Sirop de Glucose");
-        grimBlondeIngredients.add("Malte de blé");
-        grimBlondeIngredients.add("Caramel aromatique");
-        grimBlondeIngredients.add("Extraits de Houblon");
-        Blonde grimBlonde = new Blonde("Grimbergen Blonde", "Bière d'abbaye", 6.7, 0.76, grimBlondeIngredients);
-        System.out.println("\n"+grimBlonde+"\n");
+        Recette recetteBiereDuDemon = new Recette(
+                Couleur.BLONDE,
+                "Bière du Démon",
+                "Bière Forte",
+                12,
+                3.10,
+                List.of(
+                        "Eau",
+                        "Malte d'orge",
+                        "Maïs",
+                        "Riz",
+                        "Extraits de Houblon",
+                        "Houblon"
+                )
+        );
 
-        List<String> deliriumChristmasIngredients = new ArrayList<>();
-        deliriumChristmasIngredients.add("Eau");
-        deliriumChristmasIngredients.add("Malte d'orge");
-        deliriumChristmasIngredients.add("Houblon");
-        deliriumChristmasIngredients.add("Levure");
-        Blonde deliriumChristmas = new Blonde("Delirium Christmas", "Bière Triple Forte", 10, 3.15, deliriumChristmasIngredients);
-        System.out.println("\n"+deliriumChristmas+"\n");
+        Beer biereDuDemon = brasserie.brew(recetteBiereDuDemon);
+        System.out.println("\n" + biereDuDemon + "\n");
     }
 }
--- src/brasserie/Brasserie.java
+++ src/brasserie/Brasserie.java
@@ -21,16 +21,14 @@ public class Brasserie {
         this.nom = nom;
     }
 
-    // Méthode dédiée à la création de bière
-    private Beer brew(String couleur, String nom, String type, double degree, double prix, List<String> ingredients) {
-        return switch (couleur) {
-            case "Ambree" -> new Ambree(nom, type, degree, prix, ingredients);
-            case "Blanche" -> new Blanche(nom, type, degree, prix, ingredients);
-            case "Blonde" -> new Blonde(nom, type, degree, prix, ingredients);
-            case "Brune" -> new Brune(nom, type, degree, prix, ingredients);
-            case "Noire" -> new Noire(nom, type, degree, prix, ingredients);
-            case "Rousse" -> new Rousse(nom, type, degree, prix, ingredients);
-            default -> throw new IllegalArgumentException("Couleur de bière inconnue : " + couleur);
-        };
+    public Beer brew(Recette recette) {
+        return new Beer(
+                recette.getCouleur(),
+                recette.getNom(),
+                recette.getType(),
+                recette.getDegree(),
+                recette.getPrix(),
+                recette.getIngredients()
+        );
     }
 }
